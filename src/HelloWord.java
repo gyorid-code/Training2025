e09112bf3ff9d2b9a63cfe2c2688fdd9eea645a3
Győri Dániel <gyorid@gmail.com>
2025-12-17 11:48:20 +0100

    Comit

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -10,9 +10,9 @@ public class HelloWord {
         String name = sc.nextLine();
         System.out.println("Hello " + name);
 
-
-
-
+        System.out.println("Year of Birthday" );
+        int year = sc.nextInt();
+        System.out.println("Your Birthday is " + year);
 
 
     }
