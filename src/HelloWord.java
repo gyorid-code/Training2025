6ce0e5548b2904247a4ad1dae4e79d0f221e287d
Győri Dániel <gyorid@gmail.com>
2025-12-17 11:39:20 +0100

    First comit

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("Hello World !!   ");
+        String message = "Hello World!";
+
+        System.out.println(message);
+        System.out.println("What is your name?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
+
+
+
+
 
     }
 }
