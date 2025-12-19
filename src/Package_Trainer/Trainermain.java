e1fb64c41053403f330a2757e50c7a35879f5181
Győri Dániel <gyorid@gmail.com>
2025-12-19 15:35:42 +0100

    Com

--- src/Package_Trainer/Trainermain.java
+++ src/Package_Trainer/Trainermain.java
@@ -40,6 +40,7 @@ public class Trainermain {
         System.out.println("Keszult: "+ date2);
 
 
+        
         int[] a ={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         for(int i=0; i<5; i++) {
             System.out.println(a[i]);
